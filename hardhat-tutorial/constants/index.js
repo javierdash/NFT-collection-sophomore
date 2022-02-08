3230429fd118b17ad039f8e7b6c4f22dfe716ea5
Javier Herzkovich <javierdash@gmail.com>
2022-02-08 16:58:41 -0300

    add all the files

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x8976b03977b371E4E0B20f5d985C3a03591e5384";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dapp-ok.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
